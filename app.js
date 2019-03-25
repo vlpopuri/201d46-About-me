74e413a324edc699e99c9f17838ea1cac4aa72d4
vijaya <vlpopuri@hotmail.com>
2019-03-24 20:24:03 -0700

    About-me finished

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 
 function question1(){
 
-var favorite_color = prompt('is nicholas\'s favorite color red');
+  var favorite_color = prompt('is vijaya\'s favorite color red');
 
-var favorite_color_correct_answer = 'no';
+  var favorite_color_correct_answer = 'no';
 
- favorite_color = favorite_color.toLowerCase();
+  favorite_color = favorite_color.toLowerCase();
 
-if(favorite_color === favorite_color_correct_answer){
-  alert('nice guess');
-} else {
-  alert('nope');
-}
+  if(favorite_color === favorite_color_correct_answer){
+    alert('nice guess');
+  } else {
+    alert('nope');
+  }
 
 }
 question1();
@@ -20,16 +20,16 @@ question1();
 
 function question2(){
 
-var favorite_game = prompt('Answer nicholas\s favorite game?');
-favorite_game = favorite_game.toUpperCase();
-// YES
+  var favorite_game = prompt('Answer vijaya\'s favorite game?');
+  favorite_game = favorite_game.toUpperCase();
+  // YES
 
-if ( favorite_game === 'BASEBALL' || favorite_game ==='FOOTBALL') {
-  alert('you guessed Y');
- } else if(favorite_game === 'HOCKEY') {
-  alert(' well, no way, not my game');
-  } else { 
-   alert ('can we watch something else');
+  if ( favorite_game === 'BASEBALL' || favorite_game ==='FOOTBALL') {
+    alert('you guessed Y');
+  } else if(favorite_game === 'HOCKEY') {
+    alert(' well, no way, not my game');
+  } else {
+    alert ('can we watch something else');
   }
 }
 
@@ -74,11 +74,11 @@ question4();
 
 function question5(){
 
-  var favorite_touristplace = prompt('is Mexico your favorite tourist destination?');
-  var favorite_touristplace_correct_answer = 'YES';
+  var favorite_touristplace = prompt('What your favorite tourist destination?');
+  //var favorite_touristplace_correct_answer = 'YES';
 
   favorite_touristplace = favorite_touristplace.toUpperCase();
-  if( favorite_touristplace === 'Mexico'  || favorite_touristplace === 'France') {
+  if( favorite_touristplace === 'MEXICO' || favorite_touristplace === 'FRANCE') {
     alert('Great place to visit');
   } else if(favorite_touristplace === 'unknown') {
     alert('No idea?');
@@ -93,26 +93,27 @@ question5();
 
 function question6(){
 
-var favorite_number_correct_answer = '3';
+  var favorite_number_correct_answer = '3';
 
-for (var i = 0; i < 5; i++) {
-  var favorite_number = prompt('what is my favorite number?');
-  if( favorite_number === favorite_number_correct_answer ) {
-    alert(' you got that right');
-    
+  for (var i = 0; i < 5; i++) {
+    var favorite_number = prompt('what is my favorite number?');
+    if( favorite_number === favorite_number_correct_answer ) {
+      alert(' you got that right');
+      break;
+
+    }
+    else if (favorite_number > favorite_number_correct_answer)
+    {
+      alert('The number guessed is high');
+
+    }
+
+    else {
+
+      alert ('The number guessed is low');
+    }
+
   }
-  else if (favorite_number > favorite_number_correct_answer)
-  {
-   alert('The number guessed is high');
-
-  }
-
-  else  {
-
-    alert ('The number guessed is low');
-  }
-
-}
 }
 
 question6();
@@ -130,38 +131,38 @@ var user_response;
 var n;
 var m;
 
-for(var i = 0; i < ice_creams.length; i++){
-  
+for(var j = 0; j < ice_creams.length; j++){
+
   user_response=prompt('Enter your first icecream');
-  
-  while(user_response === '' || user_response === null){
+
+  while(user_response === '' || user_response === null ){
     // alert('give me an answer');
     user_response = prompt('i said give me an answer');
   }
 
- // find the index of the first ice cream if it exists in the array
+  // find the index of the first ice cream if it exists in the array
   n= ice_creams.indexOf(user_response);
   if (n>=0){
-   alert ('you guessed the first icecream right.')
+    alert ('you guessed the first icecream right.');
 
     user_response=prompt('guess the second icecream');
-   // user_response=user_response.toLowerCase;
+    // user_response=user_response.toLowerCase;
     m=ice_creams.indexOf(user_response);
 
     if (m>=0)
-    {alert ('Your guesses were : ' +  ice_creams [n] +  ' and ' + ice_creams[m]);
-    console.log(ice_creams [n]) ;
-    console.log (ice_creams [m]);
-    
-       break;
-    }
-      
+    {alert ('Your guesses were : ' + ice_creams [n] + ' and ' + ice_creams[m]);
+      console.log(ice_creams [n]) ;
+      console.log (ice_creams [m]);
 
-    else { alert ('you did not guess the second icecream. Try again')}
-    
+      break;
+    }
+
+
+    else { alert ('you did not guess the second icecream. Try again');}
+
   }
 
- // else {alert (' Your guesses were incorrect');}
+  // else {alert (' Your guesses were incorrect');}
 }
 
 // do {
@@ -172,7 +173,7 @@ for(var i = 0; i < ice_creams.length; i++){
 // }
 
 
-  
+
 
 
 
